1a981c45cfe35d31f5cd28c5c5fae860c1d1ac35
Hanna366 <2301111318@student.buksu.edu.ph>
2026-01-27 19:49:35 +0800

    Revert "coverted the the rest of the system into laravel"
    This reverts commit 466fef14cd840f53476be1847586e1ab209fd132.

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DryingRackController;
-
-Route::get('/', [DryingRackController::class, 'index'])->name('home');
-Route::post('/drying-rack/control', [DryingRackController::class, 'controlRack'])->name('drying-rack.control');
-Route::post('/drying-rack/auto-mode', [DryingRackController::class, 'toggleAutoMode'])->name('drying-rack.auto-mode');
